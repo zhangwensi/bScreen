79d04b5a464bb2a608bd1a71d4258b9567576d94
zhangwensi <1248018887@qq.com>
2021-10-10 08:50:24 +0800

    初始化项目

---
+++ src/components/B.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+import bus from '../utils/bus'
+export default {
+  name: 'B',
+  data() {
+    return {
+      message:'这是组件B'
+    }
+  },
+  mounted(){
+    bus.$on('test',(val) => {
+        console.log(val)
+        this.message = val
+    })
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <button @click="busFn">按钮</button>
+  </div>
+</template>
+
+<script>
+import bus from '../utils/bus'
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg:'这是组件A中的信息'
+    }
+  },
+  methods: {
+    busFn() {
+      console.log('aaa')
+      bus.$emit('test',this.msg)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
